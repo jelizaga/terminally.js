454067e326c014518ab36e1e0ca9022aa58ac1f0
Joel Elizaga <jelizaga@Joels-MacBook-Pro.local>
2020-01-15 11:46:19 -0800

    README now documents core functionality for me to implement in the actual library.

--- README.md
+++ README.md
@@ -1,5 +1,52 @@
 # terminally.js
-Spruce up your site with my JavaScript library for *emulating the sequential typing of a terminal!*
+Spruce up your site with my JavaScript library for *emulating terminal-like behavior!*
 
 **Depends on jQuery.js.**
 
+## How-To
+**terminally.js** is pretty straightforward.
+
+Pick any ID'd HTML element as your terminal. For example, a `<div>` with `id='terminal'`.
+
+Imagine this simulated 'terminal' as a stage. Write the 'script' for the terminal's animation using
+the library's commands (terminalInput(), terminalInputLine(), and terminalOutput()), then order the
+animation to run with **runTerminal().**
+
+### terminalInput()
+Simulate terminal user input with the terminalInput() function:
+
+### terminalInputLine()
+Simulate terminal user input and line execution with the terminalInputLine() function:
+
+### terminalOutput()
+Simulate a line of terminal output with the terminalOutput() function:
+
+### runTerminal()
+Once you've written your terminal's 'script' with the above commands, execute the runTerminal()
+command to have it all perform sequentially!
+
+## Misc.
+Make sure your **terminally.js** text is in a **monospace font** for maximum authenticity. Or don't,
+if you're feeling creative.
+
+**terminally.js** doesn't have to be used to solely simulate a terminal; you can use it for other
+text animation effects if you're feeling creative.
+
+## License
+```Copyright 2020 Joel Elizaga
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+```
--- terminally.js
+++ terminally.js
@@ -1,13 +1,78 @@
-// DEFAULTS ///////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// terminally.js ///////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// AUTHOR: Joel Elizaga, 2020
+// DESCRIPTION: terminally.js is a JavaScript library intended for simulating terminal-like
+// animation behavior in HTML elements of your choice. Have fun!
+// LICENSE: MIT
+//
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction, 
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or 
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// DEFAULTS ////////////////////////////////////////////////////////////////////////////////////////
+// Can be modified by end-user, but these are the defaults.
 const DEFAULT_TYPING_DELAY = 50;
 const DEFAULT_CURSOR = "&block;"
 const DEFAULT_C = 0;
 const DEFAULT_STR = "";
-var lineCnt = 1;
+// lineCnt
+// Beginning at 0, how many lines have been added to a terminal. Used for creating unique IDs per 
+// line.
+var lineCnt = 0;
+// terminalScript
+// Object containing the 'script' of the terminal to be ran upon the execution of runTerminal().
+var terminalScript;
 
-function type(term, txt, str, dir, typingDelay, c) {
-	lineID = term.substring(1) + "-line-" + lineCnt;
+// terminalInput
+// Simulates user input of the given text into the given terminal.
+function terminalInput(terminalID, txt, str, dir, typingDelay, c) {
+	// We're adding to the last line typed, so we grab the last line's unique ID.
+	lineID = terminalID.substring(1) + "-line-" + lineCnt;
+
+	if (typeof str == "undefined") {
+		str = DEFAULT_STR;
+	}
+	if (typeof c == "undefined") {
+		c = DEFAULT_C;
+	}
+	if (typeof typingDelay == "undefined") {
+		typingDelay = DEFAULT_TYPING_DELAY;
+	}
+	if (typeof cursor == "undefined") {
+		cursor = DEFAULT_CURSOR;
+	}
+	setTimeout(terminalTypeAnimation, typingDelay, lineID, txt, str, c, cursor, typingDelay);
+}
+
+function runTerminal() {
+	for (terminalLine in terminalScript) {
+
+	}
+}
+
+// terminalInputLine
+// Simulates user input of the given text into the given terminal, then starts a new line.
+function terminalInputLine(term, txt, str, dir, typingDelay, c) {
+	// Append new line to the specified term with a unique ID.
 	lineCnt++;
+	lineID = term.substring(1) + "-line-" + lineCnt;
 	$(term).append("<p id='" + lineID + "'></p>");
 
 	if (typeof str == "undefined") {
@@ -24,6 +89,16 @@ function type(term, txt, str, dir, typingDelay, c) {
 	}
 	
 	setTimeout(terminalTypeAnimation, typingDelay, lineID, txt, str, c, cursor, typingDelay);
+}
+
+// terminalOutput
+// 
+function terminalOutputLine(term, txt) {
+	// Append new line to the specified term with a unique ID.
+	lineCnt++;
+	lineID = term.substring(1) + "-line-" + lineCnt;
+	lineCnt++;
+	$(term).append("<p id='" + lineID + "'></p>");
 }
 
 // terminalTypeAnimation ///////////////////////////////////////////////////////////////////////////
@@ -50,6 +125,6 @@ function terminalTypeAnimation(id, txt, str, c, cursor, typingDelay) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 $(document).ready(function() {
-	type("#terminally-terminal", "BUTTTTTTTT");
-	type("#terminally-terminal", "yolo")
+	terminalInputLine("#terminally-terminal", "TEST TEST TEST");
+	terminalInputLine("#terminally-terminal", "yolo")
 });
